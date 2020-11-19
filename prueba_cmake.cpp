c5b281db765bc6b946f07c6efd67c6baa1606fed
DamianGanopolsky <damian98dgg@gmail.com>
2020-11-19 15:02:40 -0300

    Agrego prueba de como usar cmake

---
+++ prueba_cmake.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Archivo a compilar por cmake" << std::endl;
+    //Una vez que compila, genera un archivo ejecutable en la carpeta
+    //build, todo lo que esta adentro de build que no sea el ejecutable
+    //lo genera automaticamente.
+
+    return 0;
+}
