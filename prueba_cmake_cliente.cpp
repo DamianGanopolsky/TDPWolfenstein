f34821fab398252e9c1854ae31619f3a20b56ee5
DamianGanopolsky <damian98dgg@gmail.com>
2020-11-24 16:09:03 -0300

    Agrego SdlWindow y SdlText

--- prueba_cmake_cliente.cpp
+++ prueba_cmake_cliente.cpp
@@ -7,7 +7,11 @@
 //#include <QLabel>
 
 #include "Prueba_sonido.h"
+#include "SdlText.h"
+//#include "SdlTexture.h"
 #include "Music.h"
+#include "SdlWindow.h"
+
 
 int main(int argc,char* argv[]){
 
@@ -19,58 +23,37 @@ int main(int argc,char* argv[]){
     //label.show();
 
     /* MUESTRO TEXTO CON SDL */
-
     bool quit = false;
     SDL_Event event;
-    
-    SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
 
-    SDL_Window* window = SDL_CreateWindow("SDL_ttf in SDL2",
-    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640,
-    480, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SdlWindow window(640,480);
 
-    TTF_Font* Sans = TTF_OpenFont("../OpenSans-Bold.ttf", 24); 
+    SdlText texto(window.getRenderer(),255,255,255);
+    texto.Load_Text("../OpenSans-Bold.ttf","Wolfenstein",24);
 
-    SDL_Color White = {255, 255, 255};  
+    SDL_Texture* Message = SDL_CreateTextureFromSurface(window.getRenderer(), texto.getSurface()); 
+    SDL_Rect Message_rect={0,0,500,500};
+    SDL_RenderCopy(window.getRenderer(), Message, NULL, &Message_rect); 
+    //SdlTexture texture(texto.getSurface(),window);
+    //texture.render(0,0,500,500,Message_rect,Message);
 
-    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "Wolfenstein", White);
-
-    SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); 
-
-    SDL_Rect Message_rect; 
-    Message_rect.x = 0;  
-    Message_rect.y = 0;
-    Message_rect.w = 500; 
-    Message_rect.h = 500;
-
-    SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
-
-    while (!quit)
-    {
+    while (!quit){
         SDL_WaitEvent(&event);
-
-        switch (event.key.keysym.sym)
-        {
+        switch (event.key.keysym.sym){    
             case SDLK_ESCAPE:
             quit = true;
             break;
         }
-
-        SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
-        SDL_RenderPresent(renderer);
+        //texture.render(0,0,500,500,Message_rect,Message);
+        //window.render();
+        SDL_RenderCopy(window.getRenderer(), Message, NULL, &Message_rect);
+        window.render();
     }
-
-    SDL_FreeSurface(surfaceMessage);
     SDL_DestroyTexture(Message);
-    TTF_CloseFont(Sans);
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_Quit();
 
-    /* PRUEBAS DE "CONCEPTO" DE MUSICA */
+
+    /* PRUEBAS DE "CONCEPTO" DE MUSICA Y SONIDOS */
 
     if(SDL_Init(SDL_INIT_AUDIO)==-1) {
         printf("SDL_Init: %s\n", SDL_GetError());
