5ddbdbddd60cf896740349b0baf4da1b3ff56163
DamianGanopolsky <damian98dgg@gmail.com>
2020-12-06 18:09:16 -0300

    Elimino codigo innecesario

--- Editor/Scene.cpp
+++ Editor/Scene.cpp
@@ -25,22 +25,8 @@ void Scene::set(){
 }
 
 void Scene::draw(position& initial_pos,position& final_pos){
-   /* SDL_Rect Message_rect2={165,620,100,80};
-    SDL_Rect Message_rect4={30,630,70,70};
-    SDL_Rect Message_rect3={0,620,1000,80};
-    SDL_Surface* tmpSurface3 = IMG_Load("../Editor/Barra2.png");
-    SDL_Texture* barTex = SDL_CreateTextureFromSurface(window.getRenderer(),tmpSurface3);
-    SDL_Surface* tmpSurface2 = IMG_Load("../Editor/Treasure.png");
-    SDL_Texture* treasureTex = SDL_CreateTextureFromSurface(window.getRenderer(),tmpSurface2);
-    SDL_Surface* tmpSurface = IMG_Load("../prueba_mapa/assets/imagen1.png");
-    SDL_Texture* playerTex = SDL_CreateTextureFromSurface(window.getRenderer(),tmpSurface);
-    SDL_RenderCopy(window.getRenderer(), barTex, NULL, &Message_rect3);
-    SDL_RenderCopy(window.getRenderer(), treasureTex, NULL, &Message_rect4); 
-    SDL_RenderCopy(window.getRenderer(), playerTex, NULL, &Message_rect2); */
-  //  SDL_Rect Message_rect={final_pos.x,final_pos.y,40,40};
     std::cout << initial_pos.x << std::endl;
     int block_frame=int(initial_pos.x)/145;
-   // SDL_Surface* tmpSurface;
     std::cout << block_frame << std::endl;
     switch(block_frame){
         case 0:
@@ -51,11 +37,7 @@ void Scene::draw(position& initial_pos,position& final_pos){
             break;
         default:
             break;
-       // case 0..100:
-
     }
-  //  SDL_Texture* treasureTex = SDL_CreateTextureFromSurface(window.getRenderer(),tmpSurface);
-   // SDL_RenderCopy(window.getRenderer(), treasure, NULL, &Message_rect); 
 }
 
 void Scene::render(){
@@ -75,18 +57,6 @@ void Scene::render(){
 }
 
 void Scene::show(){
-    /*SDL_Rect Message_rect={165,620,100,80};
-    SDL_Rect Message_rect4={30,630,70,70};
-    SDL_Rect Message_rect3={0,620,1000,80};
-    SDL_Surface* tmpSurface3 = IMG_Load("../Editor/Barra2.png");
-    SDL_Texture* barTex = SDL_CreateTextureFromSurface(window.getRenderer(),tmpSurface3);
-    SDL_Surface* tmpSurface2 = IMG_Load("../Editor/Treasure.png");
-    SDL_Texture* treasureTex = SDL_CreateTextureFromSurface(window.getRenderer(),tmpSurface2);
-    SDL_Surface* tmpSurface = IMG_Load("../prueba_mapa/assets/imagen1.png");
-    SDL_Texture* playerTex = SDL_CreateTextureFromSurface(window.getRenderer(),tmpSurface);
-    SDL_RenderCopy(window.getRenderer(), barTex, NULL, &Message_rect3);
-    SDL_RenderCopy(window.getRenderer(), treasureTex, NULL, &Message_rect4); 
-    SDL_RenderCopy(window.getRenderer(), playerTex, NULL, &Message_rect);*/ 
 }
 
 Scene::~Scene(){
--- Editor/editor_main.cpp
+++ Editor/editor_main.cpp
@@ -30,8 +30,6 @@ int main(int argc,char* argv[]){
     EditorSoundtrack musicsoundtrack;
     musicsoundtrack.load_editor_soundtrack();
     musicsoundtrack.play_editor();
-   // SDL_Event event;
-    
     SDL_Rect Message_rect2={165,620,100,80};
     SDL_Rect Message_rect4={30,630,70,70};
     SDL_Rect Message_rect3={0,620,1000,80};
@@ -48,29 +46,6 @@ int main(int argc,char* argv[]){
     window.render();
 
     while (!quit){
-        
-   /*     SDL_RenderClear(window.getRenderer());
-        SDL_PollEvent(&event);
-        switch(event.type){
-            case SDL_KEYDOWN:
-                quit=true;
-                break;
-
-            case SDL_MOUSEBUTTONDOWN:
-                textures.insert(std::make_pair(std::make_pair(event.button.x,event.button.y), playerTex));
-                break;
-        }
-        SDL_RenderCopy(window.getRenderer(), barTex, NULL, &Message_rect3);
-        SDL_RenderCopy(window.getRenderer(), treasureTex, NULL, &Message_rect4); 
-        SDL_RenderCopy(window.getRenderer(), playerTex, NULL, &Message_rect2); 
-            
-        for (auto& it: textures) {
-            int x=std::get<0>(it.first);
-            int y=std::get<1>(it.first);
-            SDL_Rect Message_rect6={x,y,100,80};
-            SDL_RenderCopy(window.getRenderer(),(it.second),NULL,&Message_rect6);
-        }
-        window.render(); */
         quit=event_handler.handleEvents(editor);
         editor.draw();
         window.render();
