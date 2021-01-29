d7969f02503b01d73a0e2a649749abee92082a21
DamianGanopolsky <damian98dgg@gmail.com>
2021-01-29 12:17:31 -0300

    Agrego clases para el manejo de sonidos y musica

---
+++ ray_casting/SdlClasses/SoundManager.h
@@ -0,0 +1,18 @@
+#include "SdlSound.h"
+#include <SDL2/SDL_mixer.h>
+#include <vector>
+#include <map>
+
+class SoundManager{
+private:
+    SdlSound gunshot;
+
+public:
+    SoundManager();
+
+    //void load_client_sounds();
+
+    void play_sound();
+
+	//~SoundManager();
+};
