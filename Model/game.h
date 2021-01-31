1d783162efcd0e8212ae9052e801c01506ec1dc4
chiabauni <chiabauni@gmail.com>
2021-01-31 19:53:43 -0300

    Arreglo para compilar

--- Model/game.h
+++ Model/game.h
@@ -32,8 +32,9 @@ class Game {
     void _notifyMovementEvent(const Id id, const Response& response);
     Response _canMove(int** map, Player& player, std::pair<int, int> next_pos);
     bool _changeCell(PlayerPosition &pos, std::pair<int, int> &next_pos);
-    ItemOpcode _getItemOpcode(std::string message);
-    bool _interactWith(Player& player, int** map, Object obj);
+    ItemOpcode _getItemOpcode(std::string message); ///falta implmentar
+    bool _interactWith(Player& player, int** map, Object obj);///falta implmentar
+    bool _getPlayerPosition(Id map_id, int init_x, int init_y, Id new_player_id);///falta implmentar
 
     public:
         Game(ClientsConnected& clients_connected, Id map_id);
