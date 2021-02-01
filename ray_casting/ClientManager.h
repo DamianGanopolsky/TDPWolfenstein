285fc273848ca929140df244391e88cd14ed3db1
DamianGanopolsky <damian98dgg@gmail.com>
2021-02-01 09:17:33 -0300

    Agrego clase encargada del manejo del cliente

---
+++ ray_casting/ClientManager.h
@@ -0,0 +1,31 @@
+#ifndef __CLIENTMANAGER_H__
+#define __CLIENTMANAGER_H__
+
+#include "Panel_window.h"
+#include "Player.h"
+#include "Player_info.h"
+#include "Player_handler.h"
+#include "ClientConnector/Receiver.h"
+#include "ClientConnector/Sender.h"
+#include "ClientConnector/ClientSocket.h"
+#include "SdlClasses/MusicSoundtrack.h"
+#include "../Common/thread.h"
+#include "ClientConnector/ReceiveController.h"
+#include "../Common/blocking_queue.h"
+#include "Client.h"
+#include <iostream>
+
+class ClientManager {
+public:
+
+private:
+};
+
+
+
+
+
+
+
+
+#endif
