394de673eef69f06dc4742200c240d7e28a7f285
DamianGanopolsky <damian98dgg@gmail.com>
2021-02-06 11:03:41 -0300

    Agrego excepciones

--- Editor/editor_main.cpp
+++ Editor/editor_main.cpp
@@ -1,5 +1,6 @@
 #include "EditorManager.h"
 #include "../Common/defs.h"
+#include "../Common/exception.h"
 
 
 
@@ -9,7 +10,18 @@ int main(int argc,char* argv[]){
 		std::cerr << "Error invalid arguments!" << std::endl;
 		return ERROR;
 	}
-    EditorManager editormanager;
-    editormanager.start();
+    try{
+        EditorManager editormanager;
+        editormanager.start();
+	}
+	catch(const Exception& e){
+		std::cerr << e.what() << std::endl;
+		return 2;
+	}
+	catch(...){
+		std::cerr << "Unknown error" << std::endl;
+		return 3;
+	}
+
     return SUCCESS;
 }
--- ray_casting/main.cpp
+++ ray_casting/main.cpp
@@ -2,17 +2,25 @@
 #include "ClientManager.h"
 #include "../Common/protocol.h"
 #include "../Common/defs.h"
+#include "../Common/exception.h"
 
 int main(int argc,char* argv[]) {
-	//START_MOVING_UP_CMD 
 	if(argc!=EXPECTED_ARGS_CLIENT){
 		std::cerr << "Error invalid arguments!" << std::endl;
 		return ERROR;
 	}
-	ClientManager clientmanager;
-	
-	clientmanager.start();
-
+	try{
+		ClientManager clientmanager;
+		clientmanager.start();
+	}
+	catch(const Exception& e){
+		std::cerr << e.what() << std::endl;
+		return 2;
+	}
+	catch(...){
+		std::cerr << "Unknown error" << std::endl;
+		return 3;
+	}
 	return SUCCESS;
 }
 
